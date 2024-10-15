0605365e4f74918d28d104a0408330ff139107f0
Mikhail Bukharov <mikhail.bukharov@gmail.com>
2024-10-15 13:17:16 +0300

    ready

---
+++ task1.py
@@ -0,0 +1,11 @@
+import requests
+import pprint
+
+params = {
+    'q': 'html'
+}
+response = requests.get('https://api.github.com', params=params)
+
+print(response.status_code)
+response_json = response.json()
+pprint.pprint(response_json)
---
+++ task2.py
@@ -0,0 +1,10 @@
+import requests
+import pprint
+
+params = {
+    'userId': 1
+}
+response = requests.get('https://jsonplaceholder.typicode.com/posts',params=params)
+
+response_json = response.json()
+pprint.pprint(response_json)
